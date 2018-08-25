d70c0bc1070553822055c872b6ceb38c071c6690
prsahu <prsahu@expedia.com>
2018-08-25 22:16:56 +0530

    Initial project add

---
+++ src/main/java/com/prashant/pspetclinic/PsPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.prashant.pspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/prashant/pspetclinic/PsPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prashant.pspetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
